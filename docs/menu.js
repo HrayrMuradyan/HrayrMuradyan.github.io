d710b05488ab2ecd2071ed9db5b0d0bfe7bf4178
Hrayr Muradyan <hrayrmuradyan20@gmail.com>
2025-04-21 20:25:09 -0700

    Added backtotop, added footer, some other changes implemented

--- docs/menu.js
+++ docs/menu.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuItem = document.querySelector('li:has(.sub-menu)');
-    const subMenu = menuItem.querySelector('.sub-menu');
-    const workLink = menuItem.querySelector('.work-link');
-  
-    menuItem.addEventListener('click', function (e) {
+  const menuItem = document.querySelector('li:has(.sub-menu)');
+  const subMenu = menuItem.querySelector('.sub-menu');
+  const workLink = menuItem.querySelector('.work-link');
+  const navLinks = document.querySelectorAll('header ul li a');
+
+  menuItem.addEventListener('click', function (e) {
       e.stopPropagation();
       subMenu.classList.toggle('show');
       menuItem.classList.toggle('active');
       workLink.classList.toggle('active');
-    });
-  
-    // Close submenu and remove active styles when clicking outside
-    document.addEventListener('click', function () {
+  });
+
+  // Add click event to all nav links
+  navLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+          // Remove 'clicked' from all
+          navLinks.forEach(l => l.classList.remove('clicked'));
+
+          // Add 'clicked' to the clicked one
+          this.classList.add('clicked');
+      });
+  });
+
+  // Close submenu and remove active styles when clicking outside
+  document.addEventListener('click', function () {
       subMenu.classList.remove('show');
       menuItem.classList.remove('active');
       workLink.classList.remove('active');
-    });
   });
+});
